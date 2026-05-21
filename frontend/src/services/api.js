7b3c443ac9c8001a60583fb422e7bbd84d4af771
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2026-05-21 09:37:56 +0530

    Update API base URLs to production endpoints

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,19 +2,20 @@ import axios from "axios";
 
 // AUTH SERVICE
 export const AuthAPI = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://eventbook-login-service.onrender.com/api"
 });
 
 // EVENT SERVICE
 export const EventAPI = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: "https://event-booking-jrvy.onrender.com/api"
 });
 
 // 🔥 attach token automatically
 EventAPI.interceptors.request.use((config) => {
+
   const token = localStorage.getItem("token");
 
-  console.log("🔐 TOKEN:", token); // DEBUG
+  console.log("🔐 TOKEN:", token);
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
