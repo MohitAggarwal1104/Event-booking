605ed40d4da3e8090e5b5f4b3703f33902053b41
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2026-05-21 12:12:04 +0530

    Update sendEmail.js

--- Backend/LoginBackend/utils/sendEmail.js
+++ Backend/LoginBackend/utils/sendEmail.js
@@ -31,20 +31,26 @@ export const sendEmail = async (
 
       {
         headers: {
+
           accept: "application/json",
+
           "api-key": process.env.BREVO_API_KEY,
+
           "content-type": "application/json"
+
         }
       }
     );
 
     console.log("EMAIL SENT SUCCESS");
 
+    console.log(response.data);
+
     return response.data;
 
   } catch (err) {
 
-    console.log("BREVO API ERROR");
+    console.log("BREVO ERROR");
 
     console.log(
       err.response?.data || err.message
