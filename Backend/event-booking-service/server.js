70b6e2523033c23bcb23ce7b0723cc5d1ecd736d
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2026-05-21 10:03:39 +0530

    Update server.js

--- Backend/event-booking-service/server.js
+++ Backend/event-booking-service/server.js
@@ -1,35 +1,51 @@
 import dotenv from "dotenv";
-dotenv.config(); // 🔥 MUST BE FIRST
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
+
 import connectDB from "./config/db.js";
 
 import eventRoutes from "./routes/eventRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
 
-const app = express(); // 🔥 create app FIRST
-app.use(express.json());
-// middleware
-app.use(cors());
-app.use(express.json());
+const app = express();
 
-// DB
+// ================= DATABASE =================
+
 connectDB();
 
-// routes
+// ================= MIDDLEWARE =================
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://event-booking-indol-ten.vercel.app"
+  ],
+  credentials: true
+}));
+
+app.use(express.json());
+
+// ================= ROUTES =================
+
 app.use("/api/event", eventRoutes);
+
 app.use("/api/booking", bookingRoutes);
+
 app.use("/api/payment", paymentRoutes);
 
-// test route
+// ================= TEST ROUTE =================
+
 app.get("/", (req, res) => {
   res.send("Event Service Running 🚀");
 });
 
-// start server
-app.listen(5001, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+// ================= START SERVER =================
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
