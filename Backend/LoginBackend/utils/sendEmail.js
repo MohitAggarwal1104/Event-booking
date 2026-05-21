937b93b24730046d150f6e610e0781bf10b32282
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2026-05-21 11:31:45 +0530

    Update sendEmail.js

--- Backend/LoginBackend/utils/sendEmail.js
+++ Backend/LoginBackend/utils/sendEmail.js
@@ -1,39 +1,48 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, text) => {
+
   try {
 
     console.log("EMAIL USER:", process.env.EMAIL_USER);
+    console.log("EMAIL PASS EXISTS:", !!process.env.EMAIL_PASS);
 
     const transporter = nodemailer.createTransport({
+
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+
+      port: 465,
+
+      secure: true,
 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-      },
-
-      tls: {
-        rejectUnauthorized: false
       }
+
     });
 
     console.log("TRANSPORT CREATED");
 
+    // VERIFY SMTP
     await transporter.verify();
 
     console.log("SMTP VERIFIED");
 
+    // SEND MAIL
     const info = await transporter.sendMail({
+
       from: process.env.EMAIL_USER,
+
       to,
+
       subject,
+
       text
+
     });
 
-    console.log("EMAIL SENT");
+    console.log("EMAIL SENT SUCCESS");
     console.log(info);
 
     return info;
